b82f065a48cd5775112b83976d36ba5798570039
ibrahim87-coder <I.Zakaria@st.ul.edu.lb>
2020-11-28 19:43:21 +0200

    5th commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ fetch('http://localhost:3000/')
          
         var y = document.createElement('div');
         
-        var content =  `<div class="col-sm-6" style="width:400px">
+        var content =  `<div class="col-" style="width:400px">
                         <img class="card-img-top" src="FFF" alt="Flowers in Chania">
                         <div class="card-body">
                         <h4 class="card-title">YYY / DDD</h4>
